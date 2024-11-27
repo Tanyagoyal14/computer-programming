028c053682d886b56e49da4aad6cda4bbef4e08e
Tanyagoyal14 <tanyagoyal448@gmail.com>
2024-11-27 09:53:42 +0530

    count the vowels

--- coding-assignment/string/string_q08.py
+++ coding-assignment/string/string_q08.py
@@ -10,18 +10,11 @@ Input: "apple"
 Output: {'a': 1, 'p': 2, 'l': 1, 'e': 1}
 '''
 # Input
-input_string = input()  # Input string
-
-# Step 1: Initialize an empty dictionary to store frequencies
+input_string = input() 
 char_frequency = {}
-
-# Step 2: Iterate through each character in the string
 for char in input_string:
-    # Update frequency count for the character
     if char in char_frequency:
         char_frequency[char] += 1
     else:
         char_frequency[char] = 1
-
-# Step 3: Output the result
 print(char_frequency)
